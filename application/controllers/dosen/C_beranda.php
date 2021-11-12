fd79e8bc8a4bf7aec8344255cf1e91b2e84ef700
Nur Syifaul Hunsa <58512859+syifa-sisop@users.noreply.github.com>
2021-11-12 18:56:24 +0700

    Add files via upload

---
+++ application/controllers/dosen/C_beranda.php
@@ -0,0 +1,15 @@
+<?php 
+
+class C_beranda extends CI_Controller{
+
+	public function index()
+	{
+
+		$this->load->view('templates/header');
+		$this->load->view('templates_dosen/sidebar');
+		$this->load->view('dosen/v_beranda');
+		$this->load->view('templates/footer');
+	}
+
+
+}
