91804e29a254c240f3c52c5256e8e1a58a5292a1
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-11-15 10:17:30 +0700

    penambahan controller matkul pada dosen

---
+++ application/controllers/dosen/C_matkul.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class C_matkul extends CI_Controller
+{
+    public function index()
+    {
+    }
+    public function Data_Mahasiswa()
+    {
+    }
+    public function Pesan()
+    {
+    }
+}
