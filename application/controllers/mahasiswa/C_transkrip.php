af8389bb78c4dc98689fc830ce1edcb264dec5c5
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-11-15 20:27:04 +0700

    Perbaikan transkrip mahasiswa

--- application/controllers/mahasiswa/C_transkrip.php
+++ application/controllers/mahasiswa/C_transkrip.php
@@ -1,16 +1,20 @@
-<?php 
+<?php
 
-class C_transkrip extends CI_Controller{
+class C_transkrip extends CI_Controller
+{
 
 	public function index()
 	{
+		$user = $this->session->userdata('username');
+		$id_mhs = $this->m_matkul->getIdMhs($user);
+		$data['matkul'] = $this->m_matkul->tampil_mhs($id_mhs);
+
 		$data['mahasiswa'] = $this->m_mahasiswa->tampil();
-		$data['matkul'] = $this->m_matkul->tampil_mhs();
-		$data['nilai'] = $this->m_nilai->tampil_nilai();
+		$data['nilai'] = $this->m_nilai->tampil_nilai($id_mhs);
 
 		$this->load->view('templates/header');
-        $this->load->view('templates_mahasiswa/sidebar');
-        $this->load->view('mahasiswa/transkrip/v_transkrip', $data);
-        $this->load->view('templates/footer');
+		$this->load->view('templates_mahasiswa/sidebar');
+		$this->load->view('mahasiswa/transkrip/v_transkrip', $data);
+		$this->load->view('templates/footer');
 	}
 }
