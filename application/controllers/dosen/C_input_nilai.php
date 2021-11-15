a82229e6cc36c9df24baa2fee0770717ed100a49
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-11-15 10:15:22 +0700

    penambahan controller baru di dosen

---
+++ application/controllers/dosen/C_input_nilai.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class C_input_nilai extends CI_Controller
+{
+    public function index()
+    {
+    }
+    public function Data_Mahasiswa()
+    {
+    }
+    public function Pesan()
+    {
+    }
+}
