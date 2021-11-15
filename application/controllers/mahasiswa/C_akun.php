3ff4960db22789a21574a0b7909d51d486d40eb0
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-11-15 10:18:15 +0700

    menambahkan model

--- application/controllers/mahasiswa/C_akun.php
+++ application/controllers/mahasiswa/C_akun.php
@@ -1,21 +1,22 @@
-<?php 
+<?php
 
-class C_akun extends CI_Controller{
+class C_akun extends CI_Controller
+{
 
 	function __construct()
 	{
 		parent::__construct();
 		$this->my_login->check_login();
-
+		$this->load->model('m_user'); // Auto load model M_Index pada fungsi construct
 	}
 
 	public function index()
 	{
 		$data['user'] = $this->m_user->tampil('user');
 		$this->load->view('templates/header');
-        $this->load->view('templates_mahasiswa/sidebar');
-        $this->load->view('mahasiswa/user/v_user',$data);
-        $this->load->view('templates/footer');
+		$this->load->view('templates_mahasiswa/sidebar');
+		$this->load->view('mahasiswa/user/v_user', $data);
+		$this->load->view('templates/footer');
 	}
 
 	public function update($id_user)
@@ -23,15 +24,15 @@ class C_akun extends CI_Controller{
 		$data['data'] = $this->m_user->getDataByID($id_user)->row();
 
 		$this->load->view('templates/header');
-        $this->load->view('templates_mahasiswa/sidebar');
-        $this->load->view('mahasiswa/user/v_update', $data);
-        $this->load->view('templates/footer');
+		$this->load->view('templates_mahasiswa/sidebar');
+		$this->load->view('mahasiswa/user/v_update', $data);
+		$this->load->view('templates/footer');
 	}
 
 	public function update_aksi()
 	{
 		$id_user = $this->input->post('id_user');
-		
+
 		$username 				= $this->input->post('username');
 		$email 				= $this->input->post('email');
 
@@ -42,12 +43,10 @@ class C_akun extends CI_Controller{
 		$update = $this->m_user->updateFile($id_user, $data);
 
 		if ($update) {
-			$this->session->set_flashdata('update_user','Data Berhasil Diupdate !!');
+			$this->session->set_flashdata('update_user', 'Data Berhasil Diupdate !!');
 			redirect(base_url('mahasiswa/C_akun'));
-		}else{
+		} else {
 			echo "Gagal";
-			}
+		}
 	}
-
-
 }
