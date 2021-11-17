7d99fe5ded9fa08b75e633d7f86919e2441a188e
Syifa3112 <58512859+syifa-sisop@users.noreply.github.com>
2021-11-17 12:46:47 +0700

    syifa penambahan fitur akun dosen

--- application/models/m_admin.php
+++ application/models/m_admin.php
@@ -13,6 +13,15 @@ class M_admin extends CI_Model
                 return $query;
     }
 
+    public function tampil_dosen()
+    {
+         $this->db->select('*');
+                $this->db->from('user ');
+                $this->db->join('dosen','user.id_user = dosen.id_user');
+                $query = $this->db->get();
+                return $query;
+    }
+
     function tambah_data($data)
     {
         $user= [
@@ -49,4 +58,55 @@ class M_admin extends CI_Model
         $spn = $this->db->delete($table, $where);
         return $spn;
     }
+
+    function tambah_dosen($data)
+    {
+        $user= [
+            'username'  =>$data['username'],
+            'password'  =>md5($data['password']),
+            'email'     =>$data['email'],
+            'level'     => 'Dosen',
+            'status'    => 'Aktif'
+        ];
+        $item = [
+            'nama'          =>$data['nama'],
+            'nidn'          =>$data['nidn']
+        ];
+
+        $this->db->trans_begin();
+        $user['id_user'] = $this->db->insert_id();
+        $this->db->insert('user', $user);
+        $item['id_user'] = $this->db->insert_id();
+        $this->db->insert('dosen', $item);
+        
+        if($this->db->trans_status()==true){
+            $this->db->trans_commit();
+            return true;
+        }else{
+            $this->db->trans_rollback();
+            return false;
+        }
+            
+    }
+
+    public function hapus_dosen($table, $where)
+    {
+        $spn = $this->db->delete($table, $where);
+        return $spn;
+    }
+
+    public function getDataByID($id_user)
+    {
+        $this->db->from('user');
+        $this->db->where('id_user', $id_user);
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function updateFile($id_user, $data)
+    {
+        $this->db->where('id_user', $id_user);
+        return $this->db->update('user', $data);
+    }
+
 }
---
+++ application/views/landing/userdosen/v_user.php
@@ -0,0 +1,121 @@
+<div id="layoutSidenav_content">
+<main>
+  
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Pengaturan Akun</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="<?php echo base_url('C_beranda') ?>">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Akun Dosen</li>
+                        </ol>
+
+                        <?php 
+                          if($this->session->flashdata('insert_dosen')){
+                              echo '<div class="alert alert-success alert-dismissible fade show" role="alert">';
+                              echo $this->session->flashdata('insert_dosen');
+                              echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>';
+                          }                          
+                          if($this->session->flashdata('update_dosen')){
+                              echo '<div class="alert alert-primary alert-dismissible fade show" role="alert">';
+                              echo $this->session->flashdata('update_dosen');
+                              echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>';
+                          }
+                          if($this->session->flashdata('hapus_dosen')){
+                              echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">';
+                              echo $this->session->flashdata('hapus_dosen');
+                              echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>';
+                          }
+                          ?>
+                        <div class="card mb-4">
+                            <div class="card-header">  
+                               <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-user-plus"></i>Tambah Data</button>                                               
+                            </div>
+                            <div class="card-body">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>Nama</th>
+                                            <th>Username</th>
+                                            <th>Password</th>
+                                            <th>Email</th>
+                                            <th>Status</th>
+                                            <th colspan="2">Aksi</th>
+                                        </tr>
+                                    </thead>
+                                   
+                                    <tbody>
+                                        <?php
+                                        foreach($dosen as $dosen):?>
+                                        <tr>
+                                            <td><?= $dosen->nama?></td>
+                                            <td><?= $dosen->username?></td>
+                                            <td><?= $dosen->password?></td>
+                                            <td><?= $dosen->email?></td>
+                                            <td><?= $dosen->status?></td>
+
+                                                <td width="20px"><?php echo anchor('C_userdosen/update/'.$dosen->id_user, 
+                                                '<div class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></div>')?></td>
+
+                                                <td width="20px"><?php echo anchor('C_userdosen/delete/'.$dosen->id_user, 
+                                                '<div class="btn btn-sm btn-danger"><i class="fas fa-trash-alt"></i></div>')?></td>
+                                                                    
+                                        </tr>
+                                        <?php endforeach;?>
+                                        </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+
+                 <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Form Tambah Data</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                            <?php echo form_open_multipart('C_userdosen/tambah_aksi'); ?>
+                              <div class="mb-3">
+                                <label for="recipient-name" class="col-form-label">Nama</label>
+                                <input type="hidden" class="form-control" name="id_dosen">
+                                <input type="text" class="form-control" name="nama" required>
+                              </div>
+
+                              <div class="mb-3">
+                                <label for="recipient-name" class="col-form-label">NIDN</label>
+                                <input type="text" class="form-control" name="nidn" >
+                              </div>
+
+                              <div class="mb-3">
+                                <label for="recipient-name" class="col-form-label">Username</label>
+                                <input type="text" class="form-control" name="username" required>
+                              </div>
+
+                              <div class="mb-3">
+                                <label for="recipient-name" class="col-form-label">Password</label>
+                                <input type="password" class="form-control" name="password" required>
+                              </div>
+
+                              <div class="mb-3">
+                                <label for="recipient-name" class="col-form-label">Email</label>
+                                <input type="text" class="form-control" name="email">
+                              </div>
+                              
+
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                        <?php echo form_close() ?>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+              
