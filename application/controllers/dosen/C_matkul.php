fd5f68291a6596516f2ab2d2127f4e1930580d9b
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-11-27 22:53:57 +0700

    Penambahan Menu Matkul Dosen

--- application/controllers/dosen/C_matkul.php
+++ application/controllers/dosen/C_matkul.php
@@ -6,10 +6,23 @@ class C_matkul extends CI_Controller
     {
         parent::__construct();
         $this->my_login->check_login();
+        $this->load->model('Dosen/Matkul_M', 'Matkul');
     }
-    
+
     public function index()
     {
+        $user = $this->session->userdata('username');
+        $id_mhs = $this->Matkul->getIdDosen($user);
+        // echo $id_mhs;
+        $data['matkul'] = $this->Matkul->tampil_kelas($id_mhs);
+        // echo "<pre>";
+        // print_r($data);
+        // echo "</pre>";
+        // $data['dosen'] = $this->m_dosen->tampil_data()->result();
+        $this->load->view('templates/header');
+        $this->load->view('templates_dosen/sidebar');
+        $this->load->view('Dosen/V_Matkul', $data);
+        $this->load->view('templates/footer');
     }
     public function Data_Mahasiswa()
     {
---
+++ application/models/Dosen/Matkul_M.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Matkul_M extends CI_Model
+{
+    public function getIdDosen($username)
+    {
+        $this->db->select("dosen.id_dosen AS id");
+        $this->db->from("dosen,user");
+        $where = "dosen.id_user = user.id_user AND user.username = '$username'";
+        $this->db->where($where);
+        $query = $this->db->get();
+        return $query->row()->id;
+    }
+    public function tampil_kelas($idDosen)
+    {
+        $db = $this->db;
+        $db->select("*");
+        $db->from("kelas,dosen,matkul");
+        $where = "dosen.nama = kelas.dosen AND kelas.id_matkul = matkul.id_matkul AND dosen.id_dosen = $idDosen";
+        $db->where($where);
+        $query = $db->get();
+        return $query->result();
+    }
+}
