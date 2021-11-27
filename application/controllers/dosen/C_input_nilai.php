a11ac42d1859de576dd91ed9d1ecf49bef3df084
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-11-27 22:56:47 +0700

    Penambahan Controller C_input_nilai

--- application/controllers/dosen/C_input_nilai.php
+++ application/controllers/dosen/C_input_nilai.php
@@ -6,15 +6,60 @@ class C_input_nilai extends CI_Controller
     {
         parent::__construct();
         $this->my_login->check_login();
+        $this->load->model('Dosen/Nilai_M', 'Nilai');
     }
-    
+
     public function index()
     {
+        $user = $this->session->userdata('username');
+        $id_mhs = $this->Nilai->getIdDosen($user);
+        // echo $id_mhs;
+        $data['matkul'] = $this->Nilai->tampil_kelas($id_mhs);
+        // echo "<pre>";
+        // print_r($data);
+        // echo "</pre>";
+        $this->load->view('templates/header');
+        $this->load->view('templates_dosen/sidebar');
+        $this->load->view('Dosen/V_Nilai', $data);
+        $this->load->view('templates/footer');
     }
-    public function Data_Mahasiswa()
+    public function Data_Mahasiswa($idMatkul)
     {
+        $mahasiswa = $this->Nilai->getMahasiswaByMatkul($idMatkul);
+        $data = [
+            'mahasiswa' => $mahasiswa,
+            'idMatkul'  => $idMatkul
+        ];
+        // echo "<pre>";
+        // print_r($data);
+        // echo "</pre>";
+        $this->load->view('templates/header');
+        $this->load->view('templates_dosen/sidebar');
+        $this->load->view('Dosen/V_Nilai_input', $data);
+        $this->load->view('templates/footer');
     }
-    public function Pesan()
+    public function inputNilaiMahasiswa()
     {
+        $transkripID = $this->input->post('transkrip');
+        // $transkripID = 12;
+        $uts = $this->input->post('uts');
+        $uas = $this->input->post('uas');
+        $nilai = $this->input->post('nilai');
+        $idMatkul = $this->input->post('idMatkul');
+
+        $data = [
+            'nilai_uts'   => $uts,
+            'nilai_uas'   => $uas,
+            'nilai' => $nilai,
+        ];
+        $this->db->where('id', $transkripID);
+        $this->db->update('transkrip', $data);
+        redirect(base_url('/Dosen/C_input_nilai/Data_Mahasiswa/' . $idMatkul));
+        // echo "<pre>";
+        // print_r($data);
+        // echo "</pre>";
+
+        // echo "Masuk";
+        // return "MAsuk";
     }
 }
