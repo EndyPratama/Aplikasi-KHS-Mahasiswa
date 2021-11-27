497e31d3d697726d59989e669b9aa85c16b6103d
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-11-27 22:55:03 +0700

    Penambahan Controller Graph

---
+++ application/controllers/dosen/C_Graph.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class C_Graph extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->my_login->check_login();
+    }
+
+    public function index()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('templates_dosen/sidebar');
+        // $this->load->view('Dosen/Matkul/V_Matkul', $data);
+        $this->load->view('templates/footer');
+    }
+    public function Data_Mahasiswa()
+    {
+    }
+    public function Pesan()
+    {
+    }
+}
