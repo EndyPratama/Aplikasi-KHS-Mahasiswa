d58434b3865868065f1e9c40e6bf2e71a8c66a1c
Syifa3112 <58512859+syifa-sisop@users.noreply.github.com>
2021-11-28 18:35:34 +0700

    Perbaharui halaman dosen pengaturan akun

--- application/controllers/dosen/C_Setting.php
+++ application/controllers/dosen/C_Setting.php
@@ -10,15 +10,42 @@ class C_Setting extends CI_Controller
 
     public function index()
     {
+        $data['user'] = $this->m_user->tampil('user');
         $this->load->view('templates/header');
         $this->load->view('templates_dosen/sidebar');
-        // $this->load->view('Dosen/Matkul/V_Matkul', $data);
+        $this->load->view('Dosen/Akun/V_akun', $data);
         $this->load->view('templates/footer');
     }
-    public function Data_Mahasiswa()
+    public function update($id_user)
     {
+        $data['data'] = $this->m_user->getDataByID($id_user)->row();
+
+        $this->load->view('templates/header');
+        $this->load->view('templates_dosen/sidebar');
+        $this->load->view('Dosen/Akun/v_update', $data);
+        $this->load->view('templates/footer');
     }
-    public function Pesan()
+
+    public function update_aksi()
     {
+        $id_user = $this->input->post('id_user');
+
+        $username               = $this->input->post('username');
+        $password           =md5($this->input->post('password'));
+        $email              = $this->input->post('email');
+
+        $data = array(
+            'username'              => $username,
+            'password'              => $password,
+            'email'             => $email
+        );
+        $update = $this->m_user->updateFile($id_user, $data);
+
+        if ($update) {
+            $this->session->set_flashdata('update_user', 'Data Berhasil Diupdate !!');
+            redirect(base_url('dosen/C_Setting'));
+        } else {
+            echo "Gagal";
+        }
     }
 }
